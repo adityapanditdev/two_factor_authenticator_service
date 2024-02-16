6240df1ff03f6613cf255becdb2b0b747f1ad545
Aditya <railswithaditya@gmail.com>
2024-02-16 16:39:54 +0530

    update migration file

--- db/migrations/20240213161115_create_users.rb
+++ db/migrations/20240213161115_create_users.rb
@@ -4,10 +4,6 @@ Sequel.migration do
       primary_key :id
       String :email, null: false
       String :password_digest, null: false
-      # Add additional columns as needed
-      # For example, you might include columns for two-factor authentication settings
-      # such as enable_2fa, secret_key, etc.
-      
       DateTime :created_at
       DateTime :updated_at
     end
